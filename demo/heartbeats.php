7af8f6f0ce3487e4bc94eb23e0c9d691706b237d
Famibelle Médhi <famibelle@users.noreply.github.com>
2015-09-16 23:07:02 +0200

    Update heartbeats.php

--- demo/heartbeats.php
+++ demo/heartbeats.php
@@ -3,13 +3,13 @@
 $datapie = array();
 $dbname = "sqlite:/var/www/PIRlog.db";
 $db = new PDO($dbname);
-$query = "SELECT strftime('%s', timestamp) epoctime FROM TxOccupationBox WHERE epoctime > (SELECT strf('%s', 'now')-900;";
+$query = "SELECT strftime('%s', timestamp), motion epoctime FROM TxOccupationBox;";
 
 $result = $db->query($query);
 $result->setFetchMode(PDO::FETCH_ASSOC);
 while ($row = $result->fetch()) {
 	extract($row);
-	$datapie[] = array("epoctime" => ($epoctime);
+	$datapie[] = array("epoctime" => $epoctime);
 }
 $data = json_encode($datapie);
 echo $data;
